5df672be019fdda279971d07dab81f0b4465aa86
Javier Uncos <javiersrfc30@gmail.com>
2024-01-04 21:24:43 -0300

    dimos estilos a los productos agregamos en el modal carrito

---
+++ src/components/common/ItemModal.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const ItemModal = ({producto,actualizarCantidad}) => {
+
+    const handleChangeCantidad = (event) => {
+        const nuevaCantidad = event.target.value;
+        actualizarCantidad(producto.id, nuevaCantidad);
+      };
+
+
+
+
+    return (
+        <>
+        <div className='itemModal my-3'>
+            <div className='containerImg-carrito'>
+                 <img src="https://images.pexels.com/photos/3738755/pexels-photo-3738755.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            </div>
+            <div className='precioProducto-carrito container'>
+               <span>{producto.nombreProducto}</span>
+               <span className='d-block mt-2  fw-bold'>${producto.precio}</span>
+            </div>
+            <div className='containerButton-carrito'>
+              <input type="number" className='w-50 inputCarrito'
+               value={producto.cantidad}
+               onChange={handleChangeCantidad}
+              />
+              <button className='btnDelete-carrito'>X</button>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default ItemModal;
--- src/components/common/ModalCarrito.jsx
+++ src/components/common/ModalCarrito.jsx
@@ -1,30 +1,46 @@
 import React, { useContext } from 'react';
-import { Modal,Button } from 'react-bootstrap';
+import "../../style/ModalCarrito.css"
+import { Modal, Button } from 'react-bootstrap';
 import { carritoContext } from '../../context/StateCarrito';
+import ItemModal from './ItemModal';
 
-const ModalCarrito = ({show,handleClose}) => {
+const ModalCarrito = ({ show, handleClose }) => {
 
-    const {carrito,setCarrito} = useContext(carritoContext)
+    const { carrito, setCarrito } = useContext(carritoContext)
+
+    const actualizarCantidad = (productId, nuevaCantidad) => {
+        setCarrito((producto) =>{
+            const nuevosProductos = carrito.map((producto) => {
+                if (producto.id === productId) {
+                  return { ...producto, cantidad: nuevaCantidad };
+                } else {
+                  return producto;
+                }
+              });
+              // Actualizar el localStorage
+              localStorage.setItem("carritoFood", JSON.stringify(nuevosProductos));
+              return nuevosProductos;
+        }
+         
+        );
+      };
 
     return (
         <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
+                <Modal.Title>Tus Productos</Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 {
-                    carrito.map((item)=>{
-                      return  <><p>{item.nombreProducto}</p></>
-                    })
+                    carrito.map((item)=>(
+                        <ItemModal producto={item} key={item.id} actualizarCantidad={actualizarCantidad}></ItemModal>
+                    ))
                 }
             </Modal.Body>
             <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={handleClose}>
-                    Save Changes
-                </Button>
+                <div className='d-flex justify-content-center align-items-center w-100'>
+                    <button className='btnCompra-carrito'>finalizar compra</button>
+                </div>
             </Modal.Footer>
         </Modal>
     );
