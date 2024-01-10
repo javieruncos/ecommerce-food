504255c68d8f169b5cb40aa1761a6d585dd58f2e
Javier Uncos <javiersrfc30@gmail.com>
2024-01-09 23:24:12 -0300

    agregamos validacion al formulario de agregar productoa

--- src/components/view/AddProduct.jsx
+++ src/components/view/AddProduct.jsx
@@ -1,63 +1,168 @@
 import React from 'react';
 import "../../style/view/AddProduct.css"
+import { useState } from 'react';
 import { Form } from 'react-bootstrap';
+import { Controller } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
 
 const AddProduct = () => {
 
-    const { register, handelSubmit, formState: { errors } } = useForm()
+    const { register, handleSubmit, control, formState: { errors } } = useForm()
+    const [visibleIngredientes, setVisibleIngredientes] = useState(3);
 
-
+    const onSubmit = (data) => {
+        console.log(data)
+    }
 
     return (
         <div>
             <h2 className='text-center my-5'>Nuevo Producto</h2>
             <div className='container w-100 mt-5'>
-                <form className='d-flex flex-column  w-75 mx-auto formAddProduct my-5'>
+                <form className='d-flex flex-column  mx-auto formAddProduct my-5' onSubmit={handleSubmit(onSubmit)}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
+                        <Form.Label>Nombre del Producto</Form.Label>
+                        <Form.Control type="text"
+                            placeholder={errors.nombreProducto?.message}
+                            {...register("nombreProducto", {
+                                required: "este campo el obligatorio",
+                                minLength: {
+                                    value: 10,
+                                    message: "la cantidad minima de caracteres es de 10 "
+                                },
+                                maxLength: {
+                                    value: 1000,
+                                    message: "la cantidad maxima de caracteres es de 200"
+                                }
+
+                            })}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Url de la imagen</Form.Label>
+                        <Form.Control type="text"
+                            {...register("imagen", {
+                                required: "este campo el obligatorio",
+                                pattern: {
+                                    value: /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/,
+                                    message: "Debe ingresar una URL valida"
+                                }
+                            })}
+                            placeholder={errors.imagen?.message}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Precio</Form.Label>
+                        <Form.Control type="text"
+                            placeholder={errors.precio?.message}
+                            {...register("precio", {
+                                required: "este campo el obligatorio",
+                                min: {
+                                    value: 50,
+                                    message: " el precio minimo es de 50 "
+                                },
+                                max: {
+                                    value: 1000,
+                                    message: " el precio maximo es de 1000"
+                                }
+
+                            })} />
+                        <Form.Text className="text-danger">
+
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
+
+
+                        {/* <Form.Select aria-label="Default select example"
+                             {...register("categoria", {
+                                required: "Debe seleccionar una categoria"
+                            })}
+                        >
+                            <option>Selecciona una Categoria</option>
+                            <option value="Hamburguesa">Hamburguesa</option>
+                            <option value="pizza">Pizza</option>
+                            <option value="pasta">Pasta</option>
+                        </Form.Select>
+                        <Form.Text className="text-danger">
+                          {errors.categoria?.message}
+                        </Form.Text> */}
                     </Form.Group>
                     <Form.Group className="mb-3 w" controlId="formBasicEmail">
                         <Form.Label>Descripcion del producto</Form.Label>
-                        <textarea name="" id="" className='w-100 form-control'></textarea>
+                        <textarea name="" id="" className='w-100 form-control'
+                            placeholder={errors.descripcion?.message}
+                            {...register("descripcion", {
+                                required: "este campo es obligatorio",
+                                minLength: {
+                                    value: 50,
+                                    message: "la cantidad minima de caracteres es de 50"
+                                },
+                                maxLength: {
+                                    value: 1000,
+                                    message: "la cantidad maxima de caracteres es de 1000"
+                                }
+                            })}
+
+                        ></textarea>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Select aria-label="Default select example">
-                            <option>Selecciona una Categoria</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </Form.Select>
+                        <Form.Label>Ingredientes</Form.Label>
+                        <div className='mb-1'>
+                            <Form.Text className="text-danger">
+                                {errors.ingredientes && errors.ingredientes.message}
+                            </Form.Text>
+                        </div>
+                        <ul>
+                            <Controller
+                                name="ingredientes"
+                                control={control} // Asegúrate de que 'control' esté disponible aquí
+                                defaultValue={[]}
+                                rules={{ required: 'ingresa al menos 1 ingrediente' }} // Agrega la regla de validación
+                                render={({ field }) => (
+                                    <>
+                                        {field.value.map((ingrediente, index) => (
+
+                                            <li key={index}>
+                                                <input
+                                                    type="text"
+                                                    {...field}
+                                                    value={ingrediente}
+                                                    className='form-control w-100 my-2'
+                                                    onChange={(e) => {
+                                                        const newIngredientes = [...field.value];
+                                                        newIngredientes[index] = e.target.value;
+                                                        field.onChange(newIngredientes);
+                                                    }}
+                                                />
+                                            </li>
+                                        ))}
+                                        <button
+                                            type="button"
+                                            onClick={() => field.onChange([...field.value, ''])}
+                                            className='btn btn-warning mt-2'
+                                        >
+                                            Agregar Ingrediente
+                                        </button>
+                                    </>
+                                )}
+                            />
+                        </ul>
                     </Form.Group>
-                    {/* <input type="text" placeholder='nombre del producto' className='formInput form-control ' />
-                    <input type="text" placeholder='url imagen' className='formInput form-control'/>
-                    <input type="number" placeholder='precio' className='formInput form-control'/>
-                    <textarea name="" id="" placeholder='descripcion del producto' className='formInput form-control'></textarea>
-                    <select name="" id="" className='formInput form-control'>
-                        <option value="hamburguesas">Hamburgesas</option>
+                    <Form.Select aria-label="Default select example"
+                        {...register("categoria", {
+                            required: "Debe seleccionar una categoria"
+                        })}
+                        defaultValue={"hamburguesa"}
+                    >
+                        
+                        <option value="Hamburguesa">Hamburguesa</option>
                         <option value="pizza">Pizza</option>
-                        <option value="pasta">Pastas</option>
-                        <option value="pollo">pollo</option>
-                    </select> */}
-                    <button className='btn btn-warning'>agregar producto</button>
+                        <option value="pasta">Pasta</option>
+                    </Form.Select>
+                    <Form.Text className="text-danger">
+                        {errors.categoria?.message}
+                    </Form.Text>
+                    <button className='btn btn-warning w-25 mt-5'>agregar producto</button>
                 </form>
             </div>
         </div>
